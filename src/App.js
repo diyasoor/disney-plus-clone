213eeacd357fdfc2ea9f793ec25e8458b96527ef
diyasoor <89120515+diyasoor@users.noreply.github.com>
2022-03-10 19:23:11 +0530

    ADD: detail page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
+// import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Detail';
+// import { render } from "react-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+  // Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      {/* Anything inside the router will always be visible */}
+      {/* Anything inside the switch will always change */}
+      <Router>
+          <Header />
+          <Routes>
+              <Route path='/detail' element={<Detail />} />
+              <Route path='/' element={<Home />} />
+          </Routes>
+          
+      </Router>
+      
     </div>
   );
 }
