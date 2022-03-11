5f145dc8167227a5f353fba42f99fc9ac1e63729
diyasoor <89120515+diyasoor@users.noreply.github.com>
2022-03-11 23:54:10 +0530

    ADD: login

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -66,10 +66,12 @@ const ImageTitle = styled.div`
     min-height: 170px;
     width: 35vw;
     min-width: 200px;
+    margin-left: -80px;
 
     img {
       width: 100%;
       height: 100%;
+      
       object-fit: contain;
     }
 `
@@ -135,4 +137,5 @@ const Description = styled.div`
     font-size: 20px;
     margin-top: 16px;
     color: rgb(249, 249, 249); 
+    max-width: 760px;
 `
